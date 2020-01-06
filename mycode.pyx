a2d73a637d2b0624d47333812eb2c8b86310f8b4
zebus3d <48990185+zebus3d@users.noreply.github.com>
2020-01-06 15:22:23 +0100

    update

--- mycode.pyx
+++ mycode.pyx
@@ -15,7 +15,7 @@ import bpy
 cdef remove_mesh_from_meshes(meshes, fake_users):
     print("Clear orphan meshes...")
     for mesh in meshes:
-        if mesh.users < fake_users:
+        if mesh.users == fake_users:
             print("Delete: ", mesh.name, " now")
             bpy.data.meshes.remove(mesh)
 
