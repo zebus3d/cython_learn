d399d4c765aea8e679456cc1b440678ae7cf855a
zebus3d <48990185+zebus3d@users.noreply.github.com>
2020-01-06 15:21:05 +0100

    update

--- test_script_in_blender.py
+++ test_script_in_blender.py
@@ -1,11 +1,17 @@
-import sys, os.path
-sys.path.append(os.getcwd() + '/github/cython_learn/')
-
 import bpy
-import mycode
+from datetime import datetime
+
+#import sys, os.path
+#sys.path.append(os.getcwd() + '/github/cython_learn/')
+from . import mycode
+
+start = datetime.now()
+
 mylist = bpy.data.meshes[:]
 # without cdef:
 #mycode.remove_mesh_from_meshes(mylist, 1)
 
 # with cdef:
 mycode.call_remove_mesh_from_meshes(mylist, 1)
+
+print("total time: ", datetime.now() - start)
